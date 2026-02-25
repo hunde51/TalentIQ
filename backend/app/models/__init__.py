316056c16610f1162ee5ffef53c9f64ac782e3e7
hunde <hundetesfa6@gmail.com>
2026-02-25 13:26:32 +0300

    int

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,35 @@
+from app.models.application_model import Application
+from app.models.audit_log_model import AuditLog
+from app.models.chat_message_model import ChatMessage
+from app.models.cover_letter_model import CoverLetter
+from app.models.job_model import Job
+from app.models.job_match_model import JobMatchResult
+from app.models.resume_feedback_model import ResumeFeedback
 from app.models.resume_parse_model import ResumeParseResult
 from app.models.resume_model import Resume
 from app.models.user_model import User
+from app.models.user_profile_model import UserProfile
+from app.models.user_security_settings_model import UserSecuritySettings
+from app.models.user_ai_preferences_model import UserAiPreferences
+from app.models.user_notification_settings_model import UserNotificationSettings
+from app.models.user_privacy_settings_model import UserPrivacySettings
+from app.models.user_session_model import UserSession
 
-__all__ = ["User", "Resume", "ResumeParseResult"]
+__all__ = [
+    "User",
+    "Resume",
+    "ResumeParseResult",
+    "ResumeFeedback",
+    "Job",
+    "JobMatchResult",
+    "CoverLetter",
+    "Application",
+    "AuditLog",
+    "ChatMessage",
+    "UserProfile",
+    "UserSecuritySettings",
+    "UserAiPreferences",
+    "UserNotificationSettings",
+    "UserPrivacySettings",
+    "UserSession",
+]
